7068177c117a697a687f95270cca33bd4cb08b66
Hamster <90362736@qq.com>
2024-11-20 18:03:00 +0800

    24/11/20 normal update

--- src/chapt6/robot_description/launch/display_robot.launch.py
+++ src/chapt6/robot_description/launch/display_robot.launch.py
@@ -1,11 +1,11 @@
 import launch
 import launch_ros
-from ament_index_python.packages import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory             # 找包库
 
 
 def generate_launch_description():
     # 获取默认路径
-    urdf_tutorial_path = get_package_share_directory('robot_description')           # 获取包路径
+    urdf_tutorial_path = get_package_share_directory('robot_description')           # 获取包路径 注意这里找的是install下的功能包（编译后的）
     default_model_path = urdf_tutorial_path + '/urdf/b_class_car/base.urdf.xacro'
     default_rviz_config_path = urdf_tutorial_path + '/rviz/robots.rviz'
     # 为 Launch 声明参数
@@ -19,10 +19,10 @@ def generate_launch_description():
         value_type=str)
     
     # 状态发布节点
-    robot_state_publisher_node = launch_ros.actions.Node(
+    robot_state_publisher_node = launch_ros.actions.Node(                   # 发布agv的状态 运行这个功能包robot_state_publisher
         package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
+        executable='robot_state_publisher',                                 # 该功能包下的可执行文件
+        parameters=[{'robot_description': robot_description}]               # 通过robot_description 传递xacro文件的内容给robot_state_publisher
     )
     # 关节状态发布节点
     joint_state_publisher_node = launch_ros.actions.Node(
@@ -35,7 +35,7 @@ def generate_launch_description():
         executable='rviz2',
         arguments=['-d', default_rviz_config_path]          # 传递命令行参数
     )
-    return launch.LaunchDescription([
+    return launch.LaunchDescription([                       # 返回一个launch描述
         action_declare_arg_mode_path,
         joint_state_publisher_node,
         robot_state_publisher_node,
